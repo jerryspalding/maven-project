839c4b82ae13a2b255f6bcf774c273df26e03530
Jerry <jerspalding@gmail.com>
2019-12-03 11:02:26 -0500

    Checkstyle fixes

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,8 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  //This is a comment to satisfy Checkstyle
+  public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
