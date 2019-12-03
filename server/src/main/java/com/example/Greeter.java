0385fd255f56cc261ed3cc2577eb71a72f655c2f
Jerry <jerspalding@gmail.com>
2019-12-03 11:30:44 -0500

    Update

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,8 +13,8 @@ public class Greeter {
   }
 
   /** 
-   * @param some comments
-   * @return
+   * @param someone comments
+   * @return greeting string
    */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
